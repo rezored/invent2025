b6de1021b3c97630639d0fad39c0dafa3edfbcdd
Kalin Dimitrov <kalin.perspecta@gmail.com>
2026-01-12 17:09:29 +0200

    Added WP section and languages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [/*tailwind()*/],
+    integrations: [tailwind()],
+    i18n: {
+        defaultLocale: "bg",
+        locales: ["bg", "en"],
+        routing: {
+            prefixDefaultLocale: false
+        }
+    },
     build: {
         format: 'file'
     }
