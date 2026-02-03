df266a2a4b610fa4065b2ccd3bcbb20210892c9f
Kalin Dimitrov <kalin.d@getlinked.io>
2026-02-03 18:38:56 +0200

    Adding new section for my sond for school tools

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}"],
+    content: ["./src/**/*.{html,js,astro}", "./*.html"],
     darkMode: 'class',
     theme: {
         extend: {
